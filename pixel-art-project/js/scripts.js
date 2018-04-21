3c95bf1644cb232927f561ab4957f0e9f3d0de24
Lindsay <lmm566@gmail.com>
2018-04-21 19:08:06 -0400

    updated pixel art project

--- pixel-art-project/js/scripts.js
+++ pixel-art-project/js/scripts.js
@@ -1,19 +1,21 @@
+//Document ready steps
 $( document ).ready( painterTool );
 $( document ).ready( makeGrid(25,25) );
 
-//set global variables
+//Set global variables
 
 var color = $("#paint-color").val();
 var color_rgb = hexToRgbA(color);
     console.log("Color is " + color);
-    console.log("RGB color is: " + JSON.stringify(color_rgb));
+    console.log("RGB color is " + JSON.stringify(color_rgb));
+var mouseDrag = false;
 
 
 //Build grid based on form entry
 
 $(".build-grid-btn" ).click(makeGrid);
 
-//Determine paint method
+//Determine paint method & paint based on form entries
 
 $("input[type='radio']").change(painterTool);
 
@@ -27,19 +29,70 @@ $("#paint-color").change(function(){
     console.log("RGB color is: " + JSON.stringify(color_rgb));
 });
 
-//Add or background image
+//Generate random background image 
 
-$(".img-link-btn" ).click(function(){
-    var imageUrl = $("#background-image").val();
+$("#random-img-btn").click(function(){
+    // $("#canvas").css('background-image', '');
+    var randomImgNum = Math.floor(Math.random()*1084) + 1;
+    var imageUrl="https://picsum.photos/400/400/?image=" + randomImgNum.toString();
+    console.log(imageUrl);
     $("#canvas").css('background-image', 'url(' + imageUrl + ')');
+    $("#img-toggle-btn").show();
+  
+
 });
 
-$(".clear-img-btn" ).click(function(){
+//Add your own image
+
+$("#img-link-btn" ).click(function(){
+    var imageUrl = $("#background-image").val();
+    var possibleExtensions = ["png","jpeg","jpg","svg","gif","webp"];
+    var imageExtension = imageUrl.split('.').pop();
+    var urlStart = imageUrl.substring(0, 4);
+    $("#img-toggle-btn").show();
+    if (possibleExtensions.includes(imageExtension.toLowerCase()) && urlStart==="http" ){
+        $("#canvas").css('background-image', 'url(' + imageUrl + ')');
+    }
+    else{
+        $("#canvas").css('background-image', 'url(' + imageUrl + ')');
+        alert("Hmmm...if you don't see an image, double check your URL and make sure that it's a full URL path pointing to an image.")
+    }
+
+});
+
+//Clear your image and clear URL from input form
+
+$("#clear-img-btn" ).click(function(){
     $("#canvas").css('background-image', '');
+    $('#background-image').val('');
+    $("#img-toggle-btn").hide();
+});
+
+//Toggle function
+
+$("#img-toggle-btn" ).click(function(){
+    $("#canvas").toggleClass("show-hide-img");
+});
+
+//TODO: Add eraser tool to color
+
+//Clear all colors from cells
+$("#clear-colors-btn").click(function(){
+    $("td").css('background-color', '');
 });
 
 
+//Print image capability using html2canvas
+//TODO: Get this to work in chrome!
 
+$('#save-btn').click(function () {
+    html2canvas(document.querySelector("#canvas")).then(canvas => {
+        var img = canvas.toDataURL();
+        window.open(img); 
+        window.print();  
+     });
+
+});
 
 
 
@@ -57,22 +110,23 @@ function hexToRgbA(hex){
     throw new Error('Bad Hex');
 }
 
+//TODO: Add mousedown (drag) capability
 
 function painterTool(){
     paintMethod = $("input[name='draw-method']:checked").val();
-    console.log("Paint method is " + paintMethod);
+    console.log("Paint method is set to " + paintMethod);
     $("table").off();
 
     if(paintMethod==="click"){
 
-        $("table").on(paintMethod, "td", function() {
+        $("table").on("click", "td", function() {
             var old_color = $(this).css('background-color');
             color = $("#paint-color").val();
             color_rgb = hexToRgbA(color);
             
-            console.log("The old color was: " + JSON.stringify(old_color));
-            console.log("New color being painted with is: " + color);
-            console.log("New RGB color is: " + JSON.stringify(color_rgb));
+            // console.log("The old color was: " + JSON.stringify(old_color));
+            // console.log("New color being painted with is: " + color);
+            // console.log("New RGB color is: " + JSON.stringify(color_rgb));
         
             if (old_color == color_rgb) {
                 $(this).css('background-color', '');
@@ -81,17 +135,49 @@ function painterTool(){
                 $(this).css('background-color', color_rgb);
                 }
             });
-        }
-    else{
 
-        $("table").on(paintMethod, "td", function() {
+            // drag to paint
+
+            
+        $("body").mousedown(function() {
+            mouseDrag = true;
+        });
+
+        $("body").mouseup(function() {
+            mouseDrag = false;  
+         });
+        
+        $("table").on("mousemove mouseover mouseenter", "td", function() {
+            if (mouseDrag) {
+                // var old_color = $(this).css('background-color');
+                color = $("#paint-color").val();
+                color_rgb = hexToRgbA(color);
+                $(this).css('background-color', color_rgb);
+                
+                // console.log("The old color was: " + JSON.stringify(old_color));
+                // console.log("New color being painted with is: " + color);
+                // console.log("New RGB color is: " + JSON.stringify(color_rgb));
+            
+                // if (old_color == color_rgb) {
+                //     $(this).css('background-color', '');
+                //     }
+                // else{
+                //     $(this).css('background-color', color_rgb);
+                //     }
+            }
+        });
+
+    }
+    else if (paintMethod==="mouseover"){
+
+        $("table").on("mouseover", "td", function() {
             var old_color = $(this).css('background-color');
             color = $("#paint-color").val();
             color_rgb = hexToRgbA(color);
             
-            console.log("The old color was: " + JSON.stringify(old_color));
-            console.log("New color being painted with is: " + color);
-            console.log("New RGB color is: " + JSON.stringify(color_rgb));
+            // console.log("The old color was: " + JSON.stringify(old_color));
+            // console.log("New color being painted with is: " + color);
+            // console.log("New RGB color is: " + JSON.stringify(color_rgb));
             
             $(this).css('background-color', color_rgb);
 
@@ -136,3 +222,20 @@ function makeGrid(y,x) {
     }
         
 }
+
+
+
+let down = false;
+$("#pixelCanvas").mousedown(function() {
+   down = true;
+});
+$("body").mouseup(function() {
+   down = false;  
+});
+
+$("#pixelCanvas").on("mouseover", "td", function (e) {
+    e.preventDefault();
+    if (e.which === 1 && down === true) {
+        $(e.target).css("background-color", colorPicker.val());
+    }
+});
